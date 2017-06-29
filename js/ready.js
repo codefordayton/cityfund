ee73e6bbddafa0a2bd8bbc6657a83d869d5bd65a
Kera Z. Watkins <kzw@acm.org>
2017-06-28 20:07:01 -0400

    map showing for leaflet

--- js/ready.js
+++ js/ready.js
@@ -1,19 +1,21 @@
-var map;
 
 $(document).ready(function(){
-   map = getDaytonMap();
-   var circle = L.circle([39.7589, -84.1916], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
+  //  map = getDaytonMap();
+
+  var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
-function getDaytonMap(){
-
-
-// img.src = "https://maps.googleapis.com/maps/api/js?center=39.7589,-84.1916&zoom=5&size=300x300&v=3.exp&sensor=false"
-  // img.src = "http://maps.googleapis.com/maps/api/staticmap?&sensor=true&visualRefresh=true";
-  return L.map('mapDayton').setView([39.7589, -84.1916], 13);
-}
+var map = L.map('mapDayton', {
+   center: [39.7589, -84.1916],
+   zoom: 13,
+   layers: [tileLayer]
+});
+//    var circle = L.circle([39.7589, -84.1916], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
+});
