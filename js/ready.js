622a8a6c2cb9660ee13a773fa475cedab78d127f
Kera Z. Watkins <kzw@acm.org>
2017-06-28 21:29:43 -0400

    added functionality to pinpoint and visualize problems

--- js/ready.js
+++ js/ready.js
@@ -1,21 +1,44 @@
+const daytonCenter = {lat: 39.7589, lng: -84.1916};
 
 $(document).ready(function(){
-  //  map = getDaytonMap();
+  var tileLayer = getTileLayer();
 
-  var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  var map = L.map('mapDayton', {
+    center: [daytonCenter.lat, daytonCenter.lng],
+    zoom: 13,
+    layers: [tileLayer]
+  });
+
+  addPinPoint(map, daytonCenter.lat, daytonCenter.lng, "green", 1000);
+  addPinPoint(map, 39.768917, -84.1429034, "red", 100);
+  addPinPoint(map, 39.7690566, -84.1433837, "red", 100);
+  addPinPoint(map, 39.7375402, -84.1465927, "red", 100);
+  addPinPoint(map, 39.7375209,   -84.1459058, "red", 100);
+
+
 });
 
-var map = L.map('mapDayton', {
-   center: [39.7589, -84.1916],
-   zoom: 13,
-   layers: [tileLayer]
+function addPinPoint(map, lat, lng, color, radius){
+  addMarker(map, lat, lng);
+  addCircle(map, lat, lng, color, radius);
+}
+
+function addCircle(map, lat, lng, color, radius){
+  var circle = L.circle([lat, lng], {
+   color: color,
+   fillColor: color,
+   fillOpacity: 0.10,
+   radius: radius
+  }).addTo(map);
+}
+
+function addMarker(map, lat, lng){
+  var marker = L.marker([lat, lng]).addTo(map);
+}
+
+function getTileLayer(){
+  return L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
-//    var circle = L.circle([39.7589, -84.1916], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(map);
-});
+}
